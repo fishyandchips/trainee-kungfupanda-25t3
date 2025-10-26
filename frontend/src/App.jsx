8129ee358d82c800325ab520ac0c8f3f7e559454
fishyandchips <161817023+fishyandchips@users.noreply.github.com>
2025-10-26 18:51:20 +1100

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+import Pages from './Pages';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Pages />
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Pages.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+const Pages = () => {
+  return (
+    <>
+      <Routes>
+
+      </Routes>
+    </>
+  )
+}
+
+export default Pages
