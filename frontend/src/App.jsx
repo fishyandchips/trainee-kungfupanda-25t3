f459383a6f7decff5039ac994b2fdc8d843798a2
Run Shan Zhou <runshanzhou1@gmail.com>
2025-10-26 20:17:16 +1100

    Added usenavigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,30 @@
 import {
   BrowserRouter as Router,
+  useNavigate,
 } from 'react-router-dom';
 
 import Pages from './Pages';
 
 function App() {
+  const navigate = useNavigate()
+
   return (
     <>
-      <Router>
-        <div className='h-4 w-full absolute bottom-0 -translate-y-4 flex gap-4 p-4'>
-          <a className='cursor-pointer hover:underline'
-          href="/">
-            Start
-          </a>
-          <a className='cursor-pointer hover:underline'
-          href="/select">
-            Select Game
-          </a>
-          <a className='cursor-pointer hover:underline'
-          href="/game">
-            Game Screen
-          </a>
+      <div className='h-4 w-full absolute bottom-0 -translate-y-4 flex gap-4 p-4'>
+        <div className='cursor-pointer hover:underline'
+        onClick={() => navigate("/")}>
+          Start
         </div>
-        <Pages />
-      </Router>
+        <div className='cursor-pointer hover:underline'
+        onClick={() => navigate("/select")}>
+          Select Game
+        </div>
+        <div className='cursor-pointer hover:underline'
+        onClick={() => navigate("/game")}>
+          Game Screen
+        </div>
+      </div>
+      <Pages />
     </>
   )
 }
